03758bc663ff9ab3a37235de789307bb0ff69e40
Piotr Jagielski <pjg@touk.pl>
2013-09-30 22:33:40 +0200

    integration test

---
+++ src/test/java/pl/pjagielski/EmbeddedJetty.java
@@ -0,0 +1,60 @@
+package pl.pjagielski;
+
+import java.io.File;
+import java.net.URI;
+
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.resource.FileResource;
+import org.eclipse.jetty.webapp.Configuration;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EmbeddedJetty {
+
+    private static Logger logger = LoggerFactory.getLogger(EmbeddedJetty.class);
+
+    private Server server;
+
+    public void start() throws Exception {
+        server = new Server(8080);
+
+        WebAppContext webAppContext = new WebAppContext();
+//        webAppContext.setDescriptor("src/test/resources/WEB-INF/web.xml");
+        webAppContext.setResourceBase("src/main/webapp");
+        webAppContext.setContextPath("/");
+        File[] mavenLibs = Maven.resolver().loadPomFromFile("pom.xml")
+                    .importCompileAndRuntimeDependencies()
+                    .resolve().withTransitivity().asFile();
+        for (File file: mavenLibs) {
+            webAppContext.getMetaData().addWebInfJar(new FileResource(file.toURI()));
+        }
+        webAppContext.getMetaData().addContainerResource(new FileResource(new File("./target/classes").toURI()));
+
+        webAppContext.setConfigurations(new Configuration[] {
+            new AnnotationConfiguration(),
+            new WebXmlConfiguration(),
+            new WebInfConfiguration()
+        });
+        webAppContext.setParentLoaderPriority(true);
+
+        server.setHandler(webAppContext);
+
+        logger.info(">>> STARTING EMBEDDED JETTY SERVER");
+        server.start();
+        
+    }
+    
+    public void stop() throws Exception{
+        server.stop();
+    }
+    
+    public URI getBaseUri(){
+        return server.getURI();
+    }
+    
+}
