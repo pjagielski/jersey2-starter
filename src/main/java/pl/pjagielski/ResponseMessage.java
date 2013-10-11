53ee35615b87f0657272e68c4cfe4abda1f3645c
Piotr Jagielski <pjg@touk.pl>
2013-10-11 22:44:44 +0200

    NotFound handled by ExecptionMapper

--- src/main/java/pl/pjagielski/ResponseMessage.java
+++ src/main/java/pl/pjagielski/ResponseMessage.java
@@ -2,15 +2,16 @@ package pl.pjagielski;
 
 import org.joda.time.DateTime;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ResponseMessage {
 
     private String message;
     private DateTime created = DateTime.now();
 
-    public ResponseMessage() {
-    }
-
-    public ResponseMessage(String message) {
+    @JsonCreator
+    public ResponseMessage(@JsonProperty("message") String message) {
         this.message = message;
     }
 
---
+++ src/main/java/pl/pjagielski/mapper/NotFoundExceptionMapper.java
@@ -0,0 +1,24 @@
+package pl.pjagielski.mapper;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import pl.pjagielski.ResponseMessage;
+
+@Provider
+public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
+
+    @Override
+    public Response toResponse(NotFoundException exception) {
+        return Response.status(NOT_FOUND)
+            .type(APPLICATION_JSON_TYPE)
+            .entity(new ResponseMessage("Resource not found"))
+            .build();
+    }
+
+}
